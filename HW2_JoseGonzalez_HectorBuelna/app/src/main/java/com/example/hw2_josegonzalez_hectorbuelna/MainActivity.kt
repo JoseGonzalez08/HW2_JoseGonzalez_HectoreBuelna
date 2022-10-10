2d09ada3e2e8846bd1e6622ef946076912c03578
JoseGonzalez08 <42353875+JoseGonzalez08@users.noreply.github.com>
2022-10-09 19:10:39 -0700

    Add files via upload

---
+++ HW2_JoseGonzalez_HectorBuelna/app/src/main/java/com/example/hw2_josegonzalez_hectorbuelna/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hw2_josegonzalez_hectorbuelna
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
